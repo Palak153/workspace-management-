5d14a799047ed3fc6e3a86361752fcc4b71b79b2
Palak Jain <186630980+Palak153@users.noreply.github.com>
2026-05-30 17:14:57 +0530

    Initial health-check

--- workspace-backend/src/main/java/com/palak/workspace/health/HealthController.java
+++ workspace-backend/src/main/java/com/palak/workspace/health/HealthController.java
@@ -1,4 +1,18 @@
 package com.palak.workspace.health;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
 public class HealthController {
+
+    @GetMapping("/health-check")
+    public Map<String, String> healthCheck(){
+        Map<String, String> response = new HashMap<>();
+        response.put("status", "Application Working");
+        return response;
+    }
 }
