180db58ac6049da266dcbb9d63d6610a2eabc41f
Palak Jain <186630980+Palak153@users.noreply.github.com>
2026-05-30 16:22:31 +0530

    Initial backend setup

---
+++ workspace-backend/src/main/java/com/palak/workspace/WorkspaceBackendApplication.java
@@ -0,0 +1,13 @@
+package com.palak.workspace;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkspaceBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkspaceBackendApplication.class, args);
+	}
+
+}
---
+++ workspace-backend/src/test/java/com/palak/workspace/WorkspaceBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.palak.workspace;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkspaceBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
