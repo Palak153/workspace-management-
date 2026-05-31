be20c3e29bcab43612e6240197a9309cc2347782
Palak Jain <186630980+Palak153@users.noreply.github.com>
2026-05-31 18:01:11 +0530

    feat(organization): add organization retrieval and validation

--- workspace-backend/src/main/java/com/palak/workspace/organization/OrganizationController.java
+++ workspace-backend/src/main/java/com/palak/workspace/organization/OrganizationController.java
@@ -2,10 +2,7 @@ package com.palak.workspace.organization;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/organization")
@@ -20,11 +17,28 @@ public class OrganizationController {
     @PostMapping("/create-organization")
     public ResponseEntity<Organization> createOrganization(@RequestBody Organization newOrganization){
         try{
-            organizationService.saveOrganization(newOrganization);
-            return new ResponseEntity<>(newOrganization, HttpStatus.CREATED);
+            Organization organization = organizationService.saveOrganization(newOrganization);
+            return new ResponseEntity<>(organization, HttpStatus.CREATED);
         } catch (Exception e){
             return new ResponseEntity<>(newOrganization, HttpStatus.BAD_REQUEST);
         }
     }
 
+    @GetMapping("/org-code/{orgCode}")
+    public ResponseEntity<?> getByOrganizationCode(@PathVariable String orgCode){
+        Organization organization = organizationService.findOrganizationByCode(orgCode);
+        if(organization != null){
+            return new ResponseEntity<>(organization, HttpStatus.OK);
+        }
+        return new ResponseEntity<>(organization, HttpStatus.NOT_FOUND);
+    }
+
+    @GetMapping("/tenant-id/{tenantId}")
+    public ResponseEntity<?> getByTenantID(@PathVariable String tenantId){
+        Organization organization = organizationService.findOrganizationByTenantID(tenantId);
+        if(organization != null){
+            return new ResponseEntity<>(organization, HttpStatus.OK);
+        }
+        return new ResponseEntity<>(organization, HttpStatus.NOT_FOUND);
+    }
 }
--- workspace-backend/src/main/java/com/palak/workspace/organization/OrganizationService.java
+++ workspace-backend/src/main/java/com/palak/workspace/organization/OrganizationService.java
@@ -3,6 +3,7 @@ package com.palak.workspace.organization;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
+import java.util.Optional;
 import java.util.UUID;
 
 @Service
@@ -16,14 +17,34 @@ public class OrganizationService {
     }
 
     public Organization saveOrganization (Organization organization){
-        organization.setCreatedAt(LocalDateTime.now());
-        organization.setStatus(OrganizationStatus.ACTIVE);
-        String tenantId = "ORG_" + UUID.randomUUID()
-                .toString()
-                .substring(0, 6)
-                .toUpperCase();
-        organization.setTenantId(tenantId);
-        return organizationRepository.save(organization);
+        Organization organizationByCode = findOrganizationByCode(organization.getOrganizationCode());
+        if (organizationByCode == null){
+            organization.setCreatedAt(LocalDateTime.now());
+            organization.setStatus(OrganizationStatus.ACTIVE);
+            String tenantId = "ORG_" + UUID.randomUUID()
+                    .toString()
+                    .substring(0, 6)
+                    .toUpperCase();
+            organization.setTenantId(tenantId);
+            return organizationRepository.save(organization);
+        }
+        throw new RuntimeException("Organization code already exists");
+    }
+    
+    public Organization findOrganizationByCode(String organizationCode){
+        Optional<Organization> organization = organizationRepository.findByOrganizationCode(organizationCode);
+        if(organization.isPresent()){
+            return organization.get();
+        }
+        return null;
+    }
+
+    public Organization findOrganizationByTenantID(String tenantId){
+        Optional<Organization> organization = organizationRepository.findByTenantId(tenantId);
+        if(organization.isPresent()){
+            return organization.get();
+        }
+        return null;
     }
 
 }
