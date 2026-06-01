c1a143efb3181cbb3150a6dbff78e7287d3849ef
Palak Jain <186630980+Palak153@users.noreply.github.com>
2026-06-01 17:04:16 +0530

    feat(user): User Repository Implemented

---
+++ workspace-backend/src/main/java/com/palak/workspace/user/UserRepository.java
@@ -0,0 +1,15 @@
+package com.palak.workspace.user;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface UserRepository extends MongoRepository<User,String> {
+
+    Optional<User> findByEmail(String email);
+
+    Optional<User> findByEmployeeId(String employeeId);
+
+    List<User> findByTenantId(String tenantId);
+}
