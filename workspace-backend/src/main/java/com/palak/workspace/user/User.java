c267c08b3ce2e1f697d3f9b4e85c7808988b4b36
Palak Jain <186630980+Palak153@users.noreply.github.com>
2026-06-01 17:01:37 +0530

    feat(user): User Entity Implemented

---
+++ workspace-backend/src/main/java/com/palak/workspace/user/User.java
@@ -0,0 +1,48 @@
+package com.palak.workspace.user;
+
+import jakarta.validation.constraints.Email;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.index.Indexed;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.time.LocalDateTime;
+
+@Document(collection = "users")
+@NoArgsConstructor
+@Data
+public class User {
+
+    @Id
+    private String id;
+
+    @Indexed
+    private String tenantId;
+
+    private String firstName;
+    private String lastName;
+
+
+    @Email(message = "enter valid email")
+    @Indexed(unique = true)
+    private String email;
+
+    private String password;
+
+    @Indexed
+    private UserRole role;
+
+    private String designation;
+
+    @Indexed(unique = true)
+    private String employeeId;
+
+    private Boolean isActive;
+
+    private LocalDateTime lastLogin;
+
+    private LocalDateTime createdAt;
+
+    private LocalDateTime updatedAt;
+}
---
+++ workspace-backend/src/main/java/com/palak/workspace/user/UserRole.java
@@ -0,0 +1,8 @@
+package com.palak.workspace.user;
+
+public enum UserRole {
+    SUPER_ADMIN,
+    ORG_ADMIN,
+    MANAGER,
+    EMPLOYEE
+    }
